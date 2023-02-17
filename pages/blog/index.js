cf4d0131c1920dd530f82c5ecfdbfa44efae0d25
s22029 <s22029@std.it-college.ac.jp>
2023-02-17 16:51:45 +0000

    3rd commit

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,9 +1,12 @@
+import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 
 function Blog () {
   return (
     <Container>
+      <Meta pageTitle='ブログ' pageDesc='ブログの記事一覧' />
+
       <Hero
         title='Blog'
         subtitle='Recent Posts'
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 
 function Home () {
   return (
     <Container>
+      <Meta />
       <Hero
         title='CUBE'
         subtitle='output'
